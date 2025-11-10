3becdb6cae2bc8607eb382b63fa6836eee2e3542
seonghyeok <chosh2001@naver.com>
2025-11-11 00:59:00 +0900

    feat: server ec2에 캐글 데이터셋 다운로드 스크립트 작성

--- ec2_server.tf
+++ ec2_server.tf
@@ -19,10 +19,14 @@ resource "aws_instance" "capstone_02_server_ec2" {
 
   user_data_replace_on_change = true
 
-  user_data = templatefile("${path.module}/script.postgres.tftpl", {
+  user_data = templatefile("${path.module}/script.server.tftpl", {
     postgres_username = var.postgres_username
     postgres_password = var.postgres_password
+    s3_bucket_name    = aws_s3_bucket.arxiv.bucket
+    kaggle_json       = file("${path.module}/kaggle.json")
   })
+
+  depends_on = [aws_s3_bucket.arxiv]
 
   tags = {
     Name = "capstone_02_server_ec2"
