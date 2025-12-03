9918d7983cdef90f384e13856709eb94ff5ac55a
seonghyeok <chosh2001@naver.com>
2025-12-03 18:06:17 +0900

    chore: private ip도 output도 나오도록

--- outputs_ec2.tf
+++ outputs_ec2.tf
@@ -8,6 +8,11 @@ output "server_public_dns" {
   description = "Public DNS of server EC2"
 }
 
+output "server_private_ip" {
+  value       = aws_instance.server_ec2.private_ip
+  description = "Private IP of server EC2"
+}
+
 output "mongodb_public_ip" {
   value       = aws_instance.mongodb_ec2.public_ip
   description = "Public IP of mongodb EC2"
@@ -18,6 +23,11 @@ output "mongodb_public_dns" {
   description = "Public DNS of mongodb EC2"
 }
 
+output "mongodb_private_ip" {
+  value       = aws_instance.mongodb_ec2.private_ip
+  description = "Private IP of mongodb EC2"
+}
+
 output "es_public_ip" {
   value       = aws_instance.es_ec2.public_ip
   description = "Public IP of Elasticsearch EC2"
@@ -26,4 +36,9 @@ output "es_public_ip" {
 output "es_public_dns" {
   value       = aws_instance.es_ec2.public_dns
   description = "Public DNS of Elasticsearch EC2"
+}
+
+output "es_private_ip" {
+  value       = aws_instance.es_ec2.private_ip
+  description = "Private IP of Elasticsearch EC2"
 }
