00b2cd682ef8145332a6366368af7239024bc79f
seonghyeok <chosh2001@naver.com>
2025-12-04 02:46:24 +0900

    chore: eip 설정 제거

--- outputs_ec2.tf
+++ outputs_ec2.tf
@@ -13,11 +13,6 @@ output "server_private_ip" {
   description = "Private IP of server EC2"
 }
 
-output "server_elastic_ip" {
-  value       = aws_eip.server_eip.public_ip
-  description = "Elastic IP of server EC2 (고정 Public IP)"
-}
-
 output "mongodb_public_ip" {
   value       = aws_instance.mongodb_ec2.public_ip
   description = "Public IP of mongodb EC2"
