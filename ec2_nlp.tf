4e1521bffa9bddd3d70000319b0c72c8c1fae07e
seonghyeok cho <65901319+whqtker@users.noreply.github.com>
2025-12-26 18:56:45 +0900

    chore: 고정 변수는 locals로 분리 (#8)
    * chore: 고정 변수는 locals로 분리
    
    * chore: 미사용 변수 제거

--- ec2_nlp.tf
+++ ec2_nlp.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "nlp_ec2" {
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.main.id]
 
-  private_ip = var.nlp_private_ip
+  private_ip = local.nlp_private_ip
 
   key_name             = var.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
